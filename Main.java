130da23406a06678c8c54cc1e4287f1337cb9761
Prabhkk <139134714+Prabhkk@users.noreply.github.com>
2023-07-24 16:52:54 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ public class Main {
         WebPageStack webPageStack = new WebPageStack();
 
         // Adding web pages to the stack
-        webPageStack.addWebPage(new WebPage("https://www.example.com/page1", "2023-07-22 10:30:00"));
-	webPageStack.addWebPage(new WebPage("https://www.example.com/page1", "2023-07-22 11:30:00"));
-	webPageStack.addWebPage(new WebPage("https://www.example.com/page2", "2023-07-22 10:25:00"));
-	webPageStack.addWebPage(new WebPage("https://www.example.com/page3", "2023-07-22 10:35:00"));
-        webPageStack.addWebPage(new WebPage("https://www.example.com/page4", "2023-07-22 11:45:00"));
-        webPageStack.addWebPage(new WebPage("https://www.example.com/page5", "2023-07-22 12:00:00"));
+        webPageStack.addWebPage(new WebPage("https://www.google.com/page1", "2023-07-22 10:30:00"));
+	webPageStack.addWebPage(new WebPage("https://www.youtube.com/page1", "2023-07-22 11:30:00"));
+	webPageStack.addWebPage(new WebPage("https://www.microsoft.com/page2", "2023-07-22 10:25:00"));
+	webPageStack.addWebPage(new WebPage("https://www.jdoodle.com/page3", "2023-07-22 10:35:00"));
+        webPageStack.addWebPage(new WebPage("https://www.amazon.com/page4", "2023-07-22 11:45:00"));
+        webPageStack.addWebPage(new WebPage("https://www.shein.com/page5", "2023-07-22 12:00:00"));
 
         // Printing the most recent web page without removing it
         WebPage mostRecentPage = webPageStack.peekMostRecentWebPage();

13a3e75230d691df915e8ea53c6006f9331d4b9f
Prabhkk <139134714+Prabhkk@users.noreply.github.com>
2023-07-24 15:26:07 -0400

    Create WebPage.java

---
+++ WebPage.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+class WebPage {
+    String url;
+    String timestamp;
+
+    public WebPage(String url, String timestamp) {
+        this.url = url;
+        this.timestamp = timestamp;
+    }
+}
